2e40dbe55a8cad86ff55e95320eb82c370c156fd
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2024-10-06 18:42:28 +0400

    77-4 Dynamic Data Fatching. 2.0

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,12 +1,23 @@
 import { getPosts } from "@/utilits/getPost";
 
+// method-2
+const getPostDetail = async (id) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+  return data;
+};
 
 const PostDetailPage = async ({ params }) => {
-  // get all the posts
-  const allPosts = await getPosts();
-  // find the specific post with their id of
-  const post = allPosts.find((post) => post.id == params.id);
 
+  /**
+   * Method-1
+    // get all the posts
+    const allPosts = await getPosts();
+    // find the specific post with their id of
+    const post = allPosts.find((post) => post.id == params.id);
+  */
+  
+  const post = await getPostDetail(params.id);
 
   return (
     <div className="mx-40 my-10 bg-gray-200 p-5">
