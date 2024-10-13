48fa6bdf78e034accb6ccbd4def2489c5fc744c1
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2024-10-13 20:05:56 +0400

    79-3 Get NextAuth Session Data in Client and Server Component.

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,52 @@
 import NextAuth from "next-auth";
+// import Credentials from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
+
 
 const handler = NextAuth({
-    session: {
-        strategy: "jwt"
-    },
-    providers: []
+  session: {
+    strategy: "jwt"
+  },
+  providers: [
+    CredentialsProvider({
+      // credentials: {
+      //   email: {
+      //     label: "Email",
+      //     type: "email",
+      //     placeholder: "Your Email",
+      //     required: true
+      //   },
+      //   password: {
+      //     label: "Password",
+      //     type: "password",
+      //     placeholder: "Enter Password",
+      //     required: true
+      //   }
+      // },
+
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Your Email",
+          required: true
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter Password",
+          required: true
+        }
+      },
+
+      async authorize(Credentials) {
+        if(!Credentials) {
+          return null;
+        }
+        return true;
+      },
+    }),
+  ],
 });
 
 export {handler as GET, handler as POST}
